485aa3857f90da7062b31cd2297e7ba2188b3744
Nabin-Flash320 <nabindangi32@gmail.com>
2024-05-12 18:05:05 +0545

    Added interface for model olperations

---
+++ ModelDB/ModelDBInterfaceHandler.go
@@ -0,0 +1,17 @@
+
+
+package ModelDB
+
+
+type ModelDBInterfaces interface {
+
+	ModelDBCreate(interface{}) error
+	ModelDBUpdate(interface{}) error
+	ModelDBDelete(interface{}) error
+	ModelDBFindByID(uint) (interface{}, error)
+	ModelDBFindAll() ([]interface{}, error)
+
+}
+
+
+
--- ModelDB/PhonebookModelDB.go
+++ ModelDB/PhonebookModelDB.go
@@ -1,26 +1,82 @@
 
 package ModelDB
 
+
 import (
     "github.com/jinzhu/gorm"
     _ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
+
 type PhonebookModel struct {
- gorm.Model
- Name  string
- Address string
- phonenumber uint64
+    gorm.Model
+    Name  string
+    Address string
+    PhoneNumber uint64 `gorm:"unique"` 
+    // database constraint enforcement is used to describe the additional information about model column
 }
- 
+
+type ModelDBPhonebookInterfaceImplementation struct {
+	db *gorm.DB
+}
+
+func CreateNewPhonebookModelInterface(db *gorm.DB) *ModelDBPhonebookInterfaceImplementation {
+
+    return &ModelDBPhonebookInterfaceImplementation{db}
+
+}
+
+func (model_implementation *ModelDBPhonebookInterfaceImplementation) ModelDBCreate(phonebook *PhonebookModel) error {
+
+    return model_implementation.db.Create(phonebook).Error 
+
+}
+
+func (model_implementation *ModelDBPhonebookInterfaceImplementation) ModelDBUpdate(phonebook *PhonebookModel) error {
+
+    return model_implementation.db.Save(phonebook).Error 
+
+}
+
+func (model_implementation *ModelDBPhonebookInterfaceImplementation) ModelDBDelete(phonebook *PhonebookModel) error {
+
+    return model_implementation.db.Delete(phonebook).Error 
+
+}
+
+func (model_implementation *ModelDBPhonebookInterfaceImplementation) ModelDBFindByID(id uint) (interface{}, error) {
+
+    var result PhonebookModel
+    if err := model_implementation.db.First(&result, id).Error; err != nil {
+
+        return nil, err 
+
+    }
+
+    return &result, nil
+
+}
+
+func (model_implementation *ModelDBPhonebookInterfaceImplementation) ModelDBFindAll() ([] interface{}, error) {
+
+    var results []PhonebookModel
+	err := model_implementation.db.Find(&results).Error
+	var interfaces []interface{}
+	for _, v := range results {
+
+		interfaces = append(interfaces, v)
+
+	}
+
+	return interfaces, err
+
+}
 
 func UserModelDBMigrationHandler() bool {
 
     db := UserModelDBCreateConnection()
-
-    db.AutoMigrate(&PhonebookModel{})
-
     defer UserModelDBCloseConnection(db)
+    db.AutoMigrate(&PhonebookModel{})
 
     return true
     
